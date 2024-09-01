a97b8ac0ba157a0b4559e507f840c42b17049ab8
Martin Lowe <martinlowe673@gmail.com>
2024-09-01 14:55:31 +0100

    post map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,9 @@ export default async function HomePage() {
   return (
 <main>
 <div className="flex flex-wrap gap-4">
+  {posts.map((post) => (
+    <div key={post.id}>{post.name}</div>
+  ))}
   {[...mockImages,...mockImages,...mockImages].map((image,index) =>(
     <div key={image.id + "-" + index} className="w-48">
     <img src={image.url} alt="image"  />
